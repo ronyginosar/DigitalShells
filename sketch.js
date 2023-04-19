0fbda47d863ce34502df596ee76818c6972d3e72
ronyginosar <rony.ginosar@gmail.com>
2023-04-19 19:25:04 +0300

    spiral ellipse achived, now control

--- sketch.js
+++ sketch.js
@@ -11,29 +11,53 @@ function setup() {
 
 function draw() {
   translate(centerX,centerY);
-  //circle
-  // x = 56;
-  // y = 46;
-  // w = 30;
-  // h = 70; 
-  // ellipse(x, y, w, h);
-  
-  //along fibonacchi curve
+  noLoop();
+
+  // settings for ellipses
+  let w = 1;
+  let h = 10; 
+  ellipseMode(CORNER);
   noFill();
   stroke(1);
-  noLoop();
+
+  // vars of ellipses
+  let spiralLoopNumber = 3*PI;
+  let spiralDensity = 0.5*PI; // the higher the more dense
+  let spiralRadiusDelta = 0.25*PI; //1.025;
+  let startingAngle = 0;
+
+  //draw ellipses along Fibonacci curve
   beginShape();
-  for (let b = 1; b < 2; b +=1) {
-    for (let a = 0; a < 50*PI; a += 0.1) {
-      [x,y] = logarithmicSpiral(b,a);
-      // push();
-      // vertex(0, 0, xValue, yValue, 0 ,TWO_PI);
-      // pop();      
+  for (let radius = 0; radius < 10; radius +=1)
+  {
+    for (let angle = startingAngle ; 
+        // angle < (spiralLoopNumber-startingAngle); 
+        angle < 250; 
+        angle += spiralDensity) 
+    {
+      [x,y] = logarithmicSpiral(radius,(angle*PI/100));
       curveVertex(x, y);
-      b*=1.025;
+      ellipse(x, y, w, h);
+      w+=2;
+      h+=2; 
+      radius+=spiralRadiusDelta; // * or +
+      // break;
+
+      // Sequence(
+        // Ellipse(
+        //   (0.1 t π / 100; t π / 100), 
+        //   (1.9 t π / 100; t π / 100), 
+        //   t π / 100), 
+        //   t, 
+        //   0, 
+        //   250)
+
+
     }
+  // break;
   }
   endShape();
+
 }
 
 
@@ -43,45 +67,6 @@ function logarithmicSpiral(radius,angle){
   y = radius * sin(angle);
   return [x,y];
 }
-
-function shell1(){
-  // https://openprocessing.org/sketch/1569578
-  for(let a=0;a<2*PI;a+=10){
-    for(let t= 0;t<9*PI;t+=0.1){ // inc controls resolution between points
-      // a = 45;
-      // t = 5;
-      x=(cos(a)+t *exp(t)*cos(t));
-      y=(cos(a)*exp(t)*sin(t));
-      push();
-      point(x,y);
-      pop();
-     
-    }
-    
-  }
-}
-
-function shell2(){
-  // https://openprocessing.org/sketch/1569578
-  for(let t= 0;t<1;t+=1){
-    // for(let a=-30;a<(2*PI-30);a+=(0.1*PI)){
-    for(let a=0;a<(2*PI);a+=(0.1*PI)){
-    // for(let a=0;a<=90;a+=10){
-      // inc controls resolution between points
-      // a = 0;
-      // t = 5;
-
-      print("a is " + a + "t is " + t);
-      x=(cos(a)*exp(t)*cos(t));
-      y=(cos(a)*exp(t)*sin(t));
-      // push();
-      point(x,y);
-      // pop();
-    }
-  }
-}
-
-
 
 
 // https://twitter.com/a_mcsquared/status/1156588986493743104
@@ -99,4 +84,73 @@ function shell2(){
 // 		)
 // 	r++
 // 	pop()
+// }
+
+
+// saved bugs:
+// // vars of ellipses
+// let spiralLoopNumber = 3*PI;
+// let spiralDensity = 0.09*PI; // the higher the more dense
+// let spiralRadiusDelta = 0.1; //1.025;
+// let startingAngle = 0;
+
+// //draw ellipses along Fibonacci curve
+// beginShape();
+// for (let radius = 0.1; radius < 1.9; radius +=0.1)
+// {
+//   for (let angle = startingAngle ; 
+//       // angle < (spiralLoopNumber-startingAngle); 
+//       angle < 250; 
+//       angle += spiralDensity) 
+//   {
+//     [x,y] = logarithmicSpiral(radius,(angle*PI/100));
+//     // curveVertex(x, y);
+//     ellipse(x, y, w, h);
+//     w+=2;
+//     h+=2; 
+//     radius+=spiralRadiusDelta; // * or +
+//     // break;
+//   }
+// }
+
+
+//
+// // vars of ellipses
+// let spiralLoopNumber = 3*PI;
+// let spiralDensity = 1;//*PI; // the higher the more dense
+// let spiralRadiusDelta = 0.1; //1.025;
+// let startingAngle = 0;
+
+// //draw ellipses along Fibonacci curve
+// beginShape();
+// for (let radius = 0.1; radius < 1.9; radius +=0.1)
+// {
+//   for (let angle = startingAngle ; 
+//       // angle < (spiralLoopNumber-startingAngle); 
+//       angle < 250; 
+//       angle += spiralDensity) 
+//   {
+//     [x,y] = logarithmicSpiral(radius,(angle*PI/100));
+//     // curveVertex(x, y);
+//     ellipse(x, y, w, h);
+//     w+=2;
+//     h+=2; 
+//     radius+=spiralRadiusDelta; // * or +
+//     // break;
+
+//     // Sequence(
+//       // Ellipse(
+//       //   (0.1 t π / 100; t π / 100), 
+//       //   (1.9 t π / 100; t π / 100), 
+//       //   t π / 100), 
+//       //   t, 
+//       //   0, 
+//       //   250)
+
+
+//   }
+// // break;
+// }
+// endShape();
+
 // }

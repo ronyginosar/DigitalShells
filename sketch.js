2b5b97cf7993b26656ff3d358c7ce9778265a016
ronyginosar <rony.ginosar@gmail.com>
2023-05-16 15:33:35 +0300

    unify for loop with w,h for growing shape

--- sketch.js
+++ sketch.js
@@ -16,52 +16,53 @@ function draw() {
   // settings for ellipses
   noFill();
   stroke(3);
+  // ellipseMode(CORNER);
 
 
   // vars of ellipses
+  w = 1;
+  h = 1;
+
 
   // TODO optional later is using the following instead of r loop:
         // radius = a*exp(k*theta)
         // curvature = (cos(alpha)/r), with k=tan(alpha)
 
+  cycle_degrees = 360; // change to lower for 'cake slices'
 
   //draw ellipses along Fibonacci curve
   beginShape();
-  // for (let radius = 0; radius < 100; radius +=1)
-  // {
-  //   // prints(radius);
-  //   push();
-  //   ellipseMode(RADIUS);
-  //   ellipse(0,0,radius);
-  //   pop();
-    for (let angle = 0 , radius = 0;
-        angle < 360,     radius < 100; // change to lower for 'cake slices'
-        angle += 10,     radius +=1) 
+    for (let angle = 0           , radius = 0;
+        angle < cycle_degrees    , radius < 100;
+        angle += 6               , radius +=1) 
     {
       // helperCircle(radius);
       rad = degreesToRadians(angle); // logarithmicSpiral is based on radians
       // convert the r,a to x,y
-      [x,y] = locationUponLogarithmicSpiral(radius,rad);
+      [x,y] = locationUponLogarithmicSpiral(radius,rad,flip=true);
       // draw spiral curve or just points:
       // curveVertex(x, y);
       point(x,y);
       // draw ellipses along curve
       // ellipse(x, y, w, h);
-      // break; // break here for straight line if double loop
+      w +=2;
+      h = w*1.8;
     }
-    // break; // break here for only 1 swirl if double loop
-  // }
   endShape();
 }
 
 function degreesToRadians(degrees){
-  // 1° × π/180 
+  // 1° * π/180 
   radians = degrees * PI/180;
   return radians;
 }
 
 function helperCircle(radius){
-  // logarithmic spiral and an expanding circle centred at the origin to demonstrate that the angle between the tangents of the two curves at the points of intersection remains constant. Produced with WxMaxima.
+  // logarithmic spiral and an expanding circle centred 
+  // at the origin to demonstrate that the angle between 
+  // the tangents of the two curves at the points of 
+  // intersection remains constant. 
+  // https://en.wikipedia.org/wiki/File:Logspiral.gif
   push();
   ellipseMode(RADIUS);
   ellipse(0,0,radius);

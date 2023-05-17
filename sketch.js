d7fe93151427b75c9295a26b9af307bad1d2982f
ronyginosar <rony.ginosar@gmail.com>
2023-05-17 17:08:19 +0300

    draw using full function & slider for alfa

--- sketch.js
+++ sketch.js
@@ -1,12 +1,41 @@
 // center position canvas
 let centerX = 0;
 let centerY = 0;
+let slider_1;
+let slider_2;
+let slider_3;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 	centerX = windowWidth*0.5;
 	centerY = windowHeight*0.5;
   background(200);
+  slider_1 = drawSlider(1);
+  // slider_2 = drawSlider(2);
+  // slider_3 = drawSlider(3);
+}
+
+function drawSlider(idx){
+  min = 0;
+  max = 80;
+  val = 10;
+  step = 1;
+  let slider = createSlider(min, max, val, step);
+  slider.position(10, 20*idx);
+  slider.style('width', '380px');
+
+  // let label = createSpan("text");
+  let label = createSpan(slider.value());
+  // label.position(slider_1.value(), slider_1.x + slider_1.width, slider_1.y);
+  label.position(slider.x + slider.width + 10, slider.y);
+  
+  let onInput = () => {
+    label.html(slider.value());
+  };
+  
+  slider.input(onInput);
+
+  return slider
 }
 
 function draw() {
@@ -29,6 +58,10 @@ function draw() {
         // curvature = (cos(alpha)/r), with k=tan(alpha)
 
   cycle_degrees = 360; // change to lower for 'cake slices'
+  // polar_slope = 10;
+  // // r = 10;
+  // let curvature = 20;
+  // r = cos(1/tan(polar_slope)) / curvature; 
 
   //draw ellipses along Fibonacci curve
   beginShape();
@@ -36,20 +69,52 @@ function draw() {
         angle < cycle_degrees    , radius < 100;
         angle += 6               , radius +=1) 
     {
+      // angle = 30; // single angle for single line
       // helperCircle(radius);
       rad = degreesToRadians(angle); // logarithmicSpiral is based on radians
       // convert the r,a to x,y
       [x,y] = locationUponLogarithmicSpiral(radius,rad,flip=true);
       // draw spiral curve or just points:
       // curveVertex(x, y);
-      point(x,y);
+      // point(x,y);
       // draw ellipses along curve
       // ellipse(x, y, w, h);
       w +=2;
       h = w*1.8;
     }
   endShape();
+
+  // vars of ellipses
+  w = 5;
+  h = 5;
+  alfa = slider_1.value();
+  beginShape();
+  // log spiral in radians is radius=a*exp(k*phi), k = tan(alpha) -> polar slope (polar slope angle)
+  // in cartesian: x=r*cos(phi)=a*e^(k*phi)*cos(phi)
+    for (let angle = 0           ;
+      angle < cycle_degrees;
+      angle += 10          ) 
+    {
+      // radius=a*exp(k*phi)
+      a = -100; // flip will be based upon a sign. "rate of increase of the spiral"
+      // alfa = slider_1.value(); // polar slope angle
+      k = tan(degreesToRadians(alfa)); // polar slope
+      phi = degreesToRadians(angle);
+      x = a*exp(k*phi)*cos(phi);
+      y = a*exp(k*phi)*sin(phi);
+      // point(x,y);
+      // curveVertex(x,y);
+      ellipse(x, y, w, h);
+      // w +=2;
+      // h = w*1.8;
+    }
+  endShape();
 }
+
+function mouseReleased() {
+  redraw(1);
+}
+
 
 function degreesToRadians(degrees){
   // 1° * π/180 
@@ -74,8 +139,6 @@ function locationUponLogarithmicSpiral(radius,angle,flip=true){
  // log spiral in radians is radius=a*exp(k*phi), k = tan(alpha) -> polar slope (polar slope angle)
  // in cartesian: x=r*cos(phi)=a*e^(k*phi)*cos(phi)
  // constant a is the rate of increase of the spiral
- 
-
 if (flip){
    // flip sin and cos for a flipped spiral
     x = radius * sin(angle);

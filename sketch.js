9aaeda03363fdbc154924cfb8b7056aef24cec3c
ronyginosar <rony.ginosar@gmail.com>
2023-05-29 13:38:30 +0300

    p5gui switched all gui, after clean

--- sketch.js
+++ sketch.js
@@ -1,27 +1,14 @@
 // center position canvas
 let centerX,centerY = 0;
-// let centerY = 0;
-// let slider_polar_slope;
-// let slider_spiral_constant;
-// let slider_cycle_degrees;
-// let slider_increments;
-// let box_shell_flip;
-// let box_shell_flip_double;
 let w_increase = 2;
 let h_increase = 1.8;
 let w,h = 1;
-// let h = 1;
-let cycle_degrees = 360; // change to lower for 'cake slices'
 let is3D = false;
 let saveGif = false;
-// let guiY = 1;
-
+  // ellipseMode(CORNER);
 
 var visible = true;
 var gui;
-// var bgColor = '#ebe6f5';
-
-
 
 let params = {
 
@@ -58,10 +45,10 @@ let params = {
 // feature "ismobile" https://p5js.org/reference/#/p5/deviceMoved
 // todo 3d 
 // todo zoom out
-// todo gui
 // todo more params?
 // todo export pngs
 // todo  - can params change within the shell?
+// todo click to disappear gui
 
 function setup() {
   if(is3D){
@@ -72,16 +59,6 @@ function setup() {
   
 	centerX = windowWidth*0.5;
 	centerY = windowHeight*0.5;
-  guiY = windowHeight*0.7 ;
-  // slider_polar_slope = drawSlider(1,0.034,50,8.34,0.01);
-  // slider_spiral_constant = drawSlider(2,0.034,50,36.34,0.01);
-  // slider_cycle_degrees = drawSlider(3, 0, 360*10, 360, 10);
-  // slider_increments = drawSlider(4, 0, 15, 3, 0.5);
-  // box_shell_flip = drawCheckBox(5, 'flip');
-  // box_shell_flip_double = drawCheckBox(6, 'double flip');
-
-  // gui = createGui();
-  // gui.addGlobals('bgColor', 'isShellFlipped', 'isShellDoubleFlip');
 
   gui = createGui();
   gui.addObject(params);
@@ -95,31 +72,9 @@ function setup() {
   // }
 }
 
-function drawSlider(idx, min=0, max=80, val=10, step=1){
-  let slider = createSlider(min, max, val, step);
-  slider.position(10, guiY + 20*idx);
-  slider.style('width', '180px');
-  
-  let label = createSpan(slider.value());
-  label.position(slider.x + slider.width + 10, slider.y);
-  let onInput = () => {
-    label.html(slider.value());
-  };
-  slider.input(onInput);
-  return slider
-}
-
-function drawCheckBox(idx, txt){
-  checkbox = createCheckbox(txt, false);
-  checkbox.changed(changeCheckBox);
-  checkbox.position(10, guiY + 20*idx+5);
-  return checkbox;
-}
-
 function draw() {
-  translate(centerX,centerY);
+  translate(centerX,centerY); // move axis system to center screen
   noLoop();
-  
   background(params.bgColor);
 
   if (is3D){
@@ -129,35 +84,16 @@ function draw() {
   // settings for ellipses
   noFill();
   stroke(3);
-  // ellipseMode(CORNER);
+  // fill(255); // for closed shape in front
 
-  // draw ellipses along Fibonacci curve
-  // drawSpiralRadiusAngle();
-  
 
+  // calculate spiral and draw shapes using the full spiral equation
   drawSpiralFullEquation();
 
-  
+  // calculate spiral and draw shapes using the simplified spiral equation
+  // drawSpiralRadiusAngle();
 }
 
-function mouseReleased() {
-  redraw();
-}
-
-// function changeCheckBox(){
-  if (box_shell_flip_double.checked())
-  {
-    isShellDoubleFlip = true;
-  } else {
-    isShellDoubleFlip = false;
-  }
-  if (box_shell_flip.checked()){
-    params.isShellFlipped = true;
-  } else {
-    params.isShellFlipped = false;
-  }
-  redraw();
-// }
 
 function degreesToRadians(degrees){
   // 1° * π/180 
@@ -225,31 +161,31 @@ function drawSpiralFullEquation(){
   w = 1; // TODO slider these
   h = 1;
 
-  cycle_degrees = params.cycle_degrees;
-  angle_increments = params.increments;
-
   beginShape();
   // log spiral in radians is radius=a*exp(k*phi), 
   
   
     for (let angle = 0     ;
-      angle < cycle_degrees;
-      angle += angle_increments ) 
+      angle < params.cycle_degrees;
+      angle += params.increments ) 
     {
       // radius=a*exp(k*phi)
       // exp(k*phi) -> ratio of the lengths between two lines that extend out from the origin. (the log part)
       spiral_constant = params.spiral_constant; // - spiral constant, initial radius
+      // need a dummy here to not interfere with gui
+      // feature make slider neg instead of flip box
 
       if (params.isShellFlipped){
         // flip is based upon the spiral constant sign
         spiral_constant *= -1; 
       }
-      polar_slope_angel = params.polar_slope; // alpha - polar slope angle - golden/fibonacci will be +- 17. "rate of increase of the spiral"
+      // polar_slope_angel = params.polar_slope; // alpha - polar slope angle - golden/fibonacci will be +- 17. "rate of increase of the spiral"
       // alfa the angle between any line R from the origin and the line tangent to the spiral which is at the point where line R intersects the spiral. α is a constant for any given logarithmic spiral.
       // k = tan(alpha) -> polar slope (polar slope angle)
 
       // Curvature = cos(alfa)/r
-      polar_slope = tan(degreesToRadians(polar_slope_angel)); // k - polar slope, growth factor
+      // polar slope angle to polar slope
+      polar_slope = tan(degreesToRadians(params.polar_slope)); // k - polar slope, growth factor
       
       // phi the angle of rotation, is located between two lines drawn from the origin to any two points on the spiral.
       phi = degreesToRadians(angle);
@@ -266,6 +202,7 @@ function drawSpiralFullEquation(){
         curveVertex(x,y);
       }
       if (params.isDrawSpiralShapes){
+        // draw ellipses along Fibonacci curve
         ellipse(x, y, w, h);
       }
       increaseWH();
